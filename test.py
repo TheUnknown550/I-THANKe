0bf1ef09d8d3c67dee83ef259558f0fadf03e3a6
TheUnknown550 <85489536+TheUnknown550@users.noreply.github.com>
2021-10-28 21:26:04 +0700

    esfwa

--- test.py
+++ test.py
@@ -1,20 +1,6 @@
-import youtube_dl
-def run():
-    video_url = input("please enter youtube video url:")
-    video_info = youtube_dl.YoutubeDL().extract_info(
-        url = video_url,download=False
-    )
-    filename = f"{video_info['title']}.mp3"
-    options={
-        'format':'bestaudio/best',
-        'keepvideo':False,
-        'outtmpl':filename,
-    }
-
-    with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download([video_info['webpage_url']])
-
-    print("Download complete... {}".format(filename))
-
-if __name__=='__main__':
-    run()
+import time
+x=int(input("how long do you want the timer to be: "))
+start=time.time()
+while time.time() - start < 60:
+    y=time.time()-start
+    print(x-y,end='\r')
