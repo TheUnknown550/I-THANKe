554dc72e05053b87493493fb20b83787f06c76bb
TheUnknown550 <85489536+TheUnknown550@users.noreply.github.com>
2021-11-01 08:31:56 +0700

    te34

--- ChatBot/ITHANKe.py
+++ ChatBot/ITHANKe.py
@@ -1,7 +1,8 @@
+import youtube_dl
 def ChatBot(Speech):
     if 'สวัสดี' in Speech:
        print("สวัสดีครับ มีอะไรให้ช่วยไหมครับ")                  
-    elif 'ตอนนี้หมอว่างไหม' in Speech or 'ตอนนี้หมอสะดวกคุยไหม' in Speech:
+    elif 'หมอ' in Speech and 'ว่าง' in Speech:
        print("กำลังติดต่อกับคุณหมอให้นะครับ")
        avi=input("คุณหมอว่างไหม")
        if avi=='yes' or avi=='Y'or avi=='y':
@@ -15,3 +16,20 @@ def ChatBot(Speech):
        print("ให้ผู้ป่วยทำท่าตามแอนิเมชัน จากนั้นแอปจะแสดงจำนวนครั้งที่ทำ และเวลาที่ใช้ในการบำบัด เพื่อที่จะปรึกษากับนักบำบัด")
     else:
         print("ขอโทษด้วยฉันไม่เข้าใจ")
+
+def ytDownload(URL):
+    video_url = URL
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url = video_url,download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False, 
+        'outtmpl':filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete... {}".format(filename))
